df53018b3137a88521ac1f51234c9c1bdc2becd8
David Lindley <dave@davelindley.com>
2018-12-29 23:45:33 -0500

    Responsive Sizing updated for chart and controls

--- src/views/Live.vue
+++ src/views/Live.vue
@@ -1,20 +1,27 @@
 <template>
     <v-container fluid>
         <h1 >Monitor Your Cook Here</h1>
-                <chart-temp></chart-temp>
+        <v-layout row>
+            <v-flex xs12 md6 lg4>
+                <chart-temp :curve-data="curveData" :time="time" class="mx-1" ></chart-temp>
+            </v-flex>
+        </v-layout>
         <v-layout row wrap>
-            <v-flex xs9>
+            <v-flex xs9 md3>
+
             <v-slider
               v-model=goalTemp
               :min="140"
               :max="215"
               prepend-icon="whatshot"
             ></v-slider>
+
           </v-flex>
 
-          <v-flex xs3>
+          <v-flex xs3 md1>
             <v-text-field
               v-model=goalTemp
+              label="Temp"
               class="mt-0"
               type="number"
               left
@@ -23,27 +30,28 @@
           </v-flex>
 
 
-            <v-flex xs9>
+            <v-flex xs9 md3>
             <v-slider
               v-model=time
               :min="1"
-              :max="14"
+              :max="16"
               prepend-icon="timer"
             ></v-slider>
           </v-flex>
 
-          <v-flex xs3>
+          <v-flex xs3 md1>
             <v-text-field
+                    label="Hours"
               v-model=time
               class="mt-0 mx-2"
               type="number"
-              left
               full-width
+              right
             ></v-text-field>
           </v-flex>
 
             <v-layout>
-            <v-flex>
+            <v-flex md>
               <v-slider
                 :tick-labels="doneness"
                 :tick-size="3"
@@ -89,7 +97,8 @@ import ChartTemp from './ChartTemp'
                     'Firmer',
                     'Tenderer',
                     'Tender'
-                ]
+                ],
+                curveData:[75, 110, 145, 165, 182, 203, 185, 140, 110, this.goalTemp]
 
 			}
 		},
