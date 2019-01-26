d7c4a9bb0555b75e2bc1f18baa4d3bafc876eff5
David Lindley <dave@davelindley.com>
2019-01-25 20:15:52 -0500

    Main Chart now live updating!

--- src/components/MainChart.vue
+++ src/components/MainChart.vue
@@ -1,72 +1,85 @@
 <template>
-<div>
-    <highcharts :options="chartOptions" :updateArgs="chartOptions.updateArgs"></highcharts>
+    <div>
+        <highcharts :options="chartOptions" :updateArgs="chartOptions.updateArgs"></highcharts>
 
-    <ul v-for="value in cook">
-        <li>{{value.timestamp.seconds}} -- {{value.temp}}</li>
-    </ul>
-
-</div>
+    </div>
 
 </template>
 
 
 <script>
-    import {Chart} from 'highcharts-vue'
-    import axios from 'axios'
-    import { db } from '../firebase.js'
+	import {Chart} from 'highcharts-vue'
+	import axios from 'axios'
+	import {db} from '../firebase.js'
 
-    export default {
-        name:"MainChart",
-        firestore(){
-        	return{
-
-                cook: db.collection('GALLAGHER_NICK').doc('8JIknOsHxb6VFrmbxA89').collection('data').orderBy('x')
-            }
+	export default {
+		name: "MainChart",
+		firestore() {
+			return {
+                //cXUJN8rX85tUerPlw0Go
+				cook: db.collection('Dave Friday Test').doc('cXUJN8rX85tUerPlw0Go').collection('data').orderBy('x')
+			}
 		},
-    components:{
-            highcharts:Chart
-    },
-        computed:{
-            chartOptions(){ return {
-        chart: {
-          type: 'area',
-            zoomType:'x'
-        },
-           subtitle: {
-                text: document.ontouchstart === undefined ?
-                        'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-            },
-        title: {
-          text: 'Live Cook'
-        },
-        series: [{
-        	name: 'Live Cook Temp',
-          //  removes all data from firebase response except x and y.
-          data: this.cook.map(({x,y}) => ({x, y})),
-          color: '#6fcd98'
-        }],
-            yAxis:{
-            labels:{
-                enabled:true,
-                padding:'0px',
-            }},
-            xAxis:{
-            type:'datetime'
-            },
-          updateArgs:[true, true, {duration: 500}]
+		components: {
+			highcharts: Chart
+		},
+		computed: {
 
-      }}}}
+			chartOptions() {
+				return {
+					chart: {
+						type: 'area',
+						zoomType: 'x'
+					},
+					events: {
+						load: this.updateData
+					},
+					subtitle: {
+						text: document.ontouchstart === undefined ?
+							'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+					},
+					title: {
+						text: 'Live Cook'
+					},
+					series: [{
+						name: 'Live Cook Temp',
+						//  removes all data from firebase response except x and y.
+						data: this.cook.map(({x, y}) => ({x, y})),
+						color: '#6fcd98',
+						turboThreshold: 10000,
+					}],
+					yAxis: {
+						labels: {
+							enabled: true,
+							padding: '0px',
+						}
+					},
+					xAxis: {
+						type: 'datetime'
+					},
+					updateArgs: [true, true, {duration: 500}]
+
+				}
+			}},
+		methods: {
+			updateData: function () {
+				this.chart.series[0].setData(this.cook, true);
+			}
+		},
+		watch: {
+			chartOptions() {
+				console.log('data updated')
+				this.updateData()
+			}
+		}
+	}
 
 </script>
 
-]
 
 
 
 <style scoped>
-
-
 
 
 </style>
