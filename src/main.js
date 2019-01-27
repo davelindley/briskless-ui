9e9c4a4b9e1bd9ac7740be8dcc78139f41a0c4e6
David Lindley <dave@davelindley.com>
2019-01-27 11:34:34 -0500

    Firebase configuration

--- src/main.js
+++ src/main.js
@@ -3,14 +3,12 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
-import axios from 'axios'
 import VueFirestore from 'vue-firestore';
 
 
 
-Vue.use(VueFirestore)
-Vue.prototype.$http = axios
-Vue.config.productionTip = false
+Vue.use(VueFirestore);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
