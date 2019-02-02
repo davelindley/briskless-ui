c18af75d1d56990507a42ae9bee3fe6b506a8be7
David Lindley <dave@davelindley.com>
2019-02-02 08:45:45 -0500

    Remove data limits

--- src/components/MainChart.vue
+++ src/components/MainChart.vue
@@ -41,7 +41,7 @@
 						data: this.cook.map(({x, y}) => ({x, y})),
 						color: '#6fcd98',
                         //this configures the maximum amount of points on the chart
-						turboThreshold: 1000000,
+						turboThreshold: 0,
 					}],
 					yAxis: {
 						labels: {
