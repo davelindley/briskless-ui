1d2b286bc3d6f65fef279d6ed070164376704ad5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-02 13:43:38 +0000

    Deploy davelindley/briskless-ui to github.com/davelindley/briskless-ui.git:gh-pages

---
+++ precache-manifest.daf3f93ba9bcc425bfdbcea234a40d5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5ab0108d90c1ed62c98a",
+    "url": "/js/chunk-vendors.9e6bba7d.js"
+  },
+  {
+    "revision": "9fef0c4d7bbd49048983",
+    "url": "/js/app.fa311c05.js"
+  },
+  {
+    "revision": "96c376c943f92eee7f95ad43056f60ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ab0108d90c1ed62c98a",
+    "url": "/css/chunk-vendors.b9f165ea.css"
+  },
+  {
+    "revision": "9fef0c4d7bbd49048983",
+    "url": "/css/app.1904a2b6.css"
+  },
+  {
+    "revision": "35af776754c431356d676fe50eb5733a",
+    "url": "/avatar-2.png"
+  }
+];
