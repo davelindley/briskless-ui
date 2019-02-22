67a32233580ba799143bc8e2dc6b61aa42919be9
David Lindley <dave@davelindley.com>
2019-02-22 18:43:21 -0500

    Layout updates for mobile/desktop. Order of 'tender' changed.

--- src/views/Live.vue
+++ src/views/Live.vue
@@ -1,6 +1,12 @@
 <template>
-  <v-container fluid>
-    <v-layout row wrap>
+  <v-container
+    fluid
+    grid-list-md
+  >
+    <v-layout
+      row
+      wrap
+    >
       <v-flex
         lg4
         md6
@@ -11,122 +17,129 @@
           class="mx-1"
         />
       </v-flex>
+
+      <v-layout
+        row
+        wrap
+      >
         <v-flex
-        xs6
-        md3
-      >
-        <gauge
-          title="Internal Temp"
-          :value="180"
-          :min="0"
-          :max="220"
-          subtitle="Deg F"
-        />
-      </v-flex>
+          xs12
+          md6
+        >
+          <h1>TIME REMAINING: {{ this.timeRemaining }}</h1>
 
-      <v-flex
-        xs6
-        md3
-      >
-        <gauge
-          title="Velocity"
-          :value="2.3"
-          :min="0"
-          :max="5"
-          subtitle="Chews"
-        />
-      </v-flex>
-
-
-
-
-
-    </v-layout>
-    <v-layout>
-
-    </v-layout>
-
-
-    <v-layout
-      justify-center
-      row
-      wrap
-    >
-      <v-flex
-        md3
-        xs10
-      >
-        <v-slider
-          v-model="goalTemp"
-          :max="215"
-          :min="140"
-          prepend-icon="whatshot"
-        />
-      </v-flex>
-
-      <v-flex
-        md1
-        xs2
-      >
-        <v-text-field
-          v-model="goalTemp"
-          class="mt-0"
-          full-width
-          label="Temp"
-          left
-          type="number"
-        />
-      </v-flex>
-
-
-      <v-flex
-        md3
-        xs10
-      >
-        <v-slider
-          v-model="time"
-          :max="16"
-          :min="1"
-          prepend-icon="timer"
-        />
-      </v-flex>
-
-      <v-flex
-        md1
-        xs2
-      >
-        <v-text-field
-          v-model="time"
-          class="mt-0 mx-2"
-          full-width
-          label="Hours"
-          px-0
-          right
-          type="number"
-        />
-      </v-flex>
-
-      <v-layout>
-        <v-flex md>
-          <v-slider
-            :tick-labels="doneness"
-            :tick-size="3"
-            :value="2"
-            max="3"
-            min="0"
-            thumb-label
-            thumb-size="80"
+          <v-layout
+            row
+            wrap
           >
-            <template
-              slot="thumb-label"
-              slot-scope="props"
+            <v-flex
+              xs6
             >
-              <span>
-                {{ getDoneness(props.value) }}
-              </span>
-            </template>
-          </v-slider>
+              <gauge
+                title="Internal Temp"
+                :value="180"
+                :min="0"
+                :max="220"
+                subtitle="Deg F"
+              />
+            </v-flex>
+            <v-flex xs6>
+              <gauge
+                title="Velocity"
+                :value="2.3"
+                :min="0"
+                :max="5"
+                subtitle="Chews"
+              />
+            </v-flex>
+            <v-layout />
+          </v-layout>
         </v-flex>
+        </v-flex>
+      </v-layout>
+
+
+      <v-layout
+        justify-center
+        row
+        wrap
+      >
+        <v-flex
+          md3
+          xs10
+        >
+          <v-slider
+            v-model="goalTemp"
+            :max="215"
+            :min="140"
+            prepend-icon="whatshot"
+          />
+        </v-flex>
+
+        <v-flex
+          md1
+          xs2
+        >
+          <v-text-field
+            v-model="goalTemp"
+            class="mt-0"
+            full-width
+            label="Temp"
+            left
+            type="number"
+          />
+        </v-flex>
+
+
+        <v-flex
+          md3
+          xs10
+        >
+          <v-slider
+            v-model="time"
+            :max="16"
+            :min="1"
+            prepend-icon="timer"
+          />
+        </v-flex>
+
+        <v-flex
+          md1
+          xs2
+        >
+          <v-text-field
+            v-model="time"
+            class="mt-0 mx-2"
+            full-width
+            label="Hours"
+            px-0
+            right
+            type="number"
+          />
+        </v-flex>
+
+        <v-layout>
+          <v-flex md>
+            <v-slider
+              :tick-labels="doneness"
+              :tick-size="3"
+              :value="2"
+              max="3"
+              min="0"
+              thumb-label
+              thumb-size="80"
+            >
+              <template
+                slot="thumb-label"
+                slot-scope="props"
+              >
+                <span>
+                  {{ getDoneness(props.value) }}
+                </span>
+              </template>
+            </v-slider>
+          </v-flex>
+        </v-layout>
       </v-layout>
     </v-layout>
   </v-container>
@@ -160,8 +173,8 @@
 				doneness: [
 					'Firm',
 					'Firmer',
-					'Tenderer',
-					'Tender'
+					'Tender',
+					'Tenderer'
 				]
 			}
 		},
@@ -169,6 +182,9 @@
 			velocity() {
 				return Number(0.00000000000000007048 * Math.pow(this.temp, 7.29007299056)).toPrecision(4)
 			},
+            timeRemaining(){
+			  return "1:40M"
+            }
 
 
 		},
