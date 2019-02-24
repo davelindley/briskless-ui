6fe828fa6fe4ea3964b096271e531018a8355d36
David Lindley <dave@davelindley.com>
2019-02-24 11:27:10 -0500

    Initial Commit - template for Sign Up component

---
+++ src/views/SignUp.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container>
+        <v-layout>
+            <v-flex>
+                <p>Create an account!</p>
+                <input placeholder="email" type="text">
+                <input placeholder="password" type="password">
+                <v-btn>Sign Up</v-btn>
+                <span>or go back to Login</span>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+	export default {
+		name: "SignUp"
+	}
+</script>
+
+<style scoped>
+
+</style>
